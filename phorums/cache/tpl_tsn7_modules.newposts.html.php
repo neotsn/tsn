c07dcba40feabce719afa4df3eda5791b931f91b
[[Neo]] at tsn <neotsn@gmail.com>
2014-03-24 22:46:40 -0500

    removing files

--- phorums/cache/tpl_tsn7_modules.newposts.html.php
+++
@@ -1,3 +0,0 @@
-<?php if (!defined('IN_PHPBB')) exit; ?><form method="post" action="<?php echo (isset($this->_rootref['S_SEARCH_ACTION'])) ? $this->_rootref['S_SEARCH_ACTION'] : ''; ?>">
-	<div id="divNewPosts"></div>
-</form>
