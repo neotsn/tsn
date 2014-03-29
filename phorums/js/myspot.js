f299d5701c523068dc7262d0316effd7e68dac6d
[[Neo]] at tsn <neotsn@gmail.com>
2014-03-28 22:43:23 -0500

    Added mini forum index to myspot page fixed some styling for the myspot modules updated login fields to show a login form on myspot if not logged in fixed bug where large login form showed up if not logged in

--- phorums/js/myspot.js
+++ phorums/js/myspot.js
@@ -4,22 +4,28 @@
 
 $(document).on('ready', function () {
 
-	// Get mini profile
-	ajax_fetch_html('module_mini_profile.php?', 'divMiniProfile', false);
+	var login_status = $("#login_status").val();
+
+	if (login_status == 1) {
+		// Get mini profile
+		ajax_fetch_html('module_mini_profile.php?', 'divMiniProfile', false);
+	}
 
 	// Get tsn Special Report
 	ajax_fetch_html('module_news.php?', 'divSpecialReport', false);
 
+	ajax_fetch_html('module_mini_index.php?', 'divMiniForumIndex', false);
+
 });
 
-function ajax_fetch_html(url, elementid, refetch) {
+function ajax_fetch_html(url, elementid, refresh) {
 	$.ajax({
 		url: url + "sid=" + Math.random()
 	}).done(function (data) {
 		$('#' + elementid).html(data);
-		if (refetch) {
+		if (refresh) {
 			setTimeout(function () {
-				ajax_fetch_html(url, elementid, refetch);
+				ajax_fetch_html(url, elementid, refresh);
 			}, 30000);
 		}
 	});
