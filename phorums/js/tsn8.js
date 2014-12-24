d66ec11b3ba89327dc62a994594bf9eef11f3298
[[Neo]] at tsn <neotsn@gmail.com>
2014-12-24 10:59:47 -0600

    fixes to javascript and html autoformatting that breaks toolbars.

--- phorums/js/tsn8.js
+++ phorums/js/tsn8.js
@@ -1,39 +1,5 @@
 $(document).ready(function () {
-	/**
-	 * Monitor New Topic button on view forum/topic/post pages
-	 */
-		//$('span.tsn8_icon_newtopic').on('click', function () {
-		//	var element_id = 'tsn8_newtopic_modal';
-		//
-		//	var id = $(this).attr('data-id');
-		//	var mode = $(this).attr('data-mode');
-		//	var param = $(this).attr('data-param');
-		//	var url = $(this).attr('data-url');
-		//
-		//	url = url + '?mode=' + mode + "&" + param + '=' + id;
-		//
-		//	make_ajax_request(url, 'GET');
-		//
-		//	var modal_box = $('#' + element_id);
-		//	var dialog = modal_box.dialog({
-		//		autoOpen: false,
-		//		height: 500,
-		//		width: 800,
-		//		modal: true,
-		//		buttons: {
-		//
-		//			"Cancel": function () {
-		//				dialog.dialog("close");
-		//			}
-		//		},
-		//		close: function () {
-		//
-		//		}
-		//	});
-		//	dialog.dialog("open");
-		//});
-
-		// Implement jQuery UI Buttons & Button Sets
+	// Implement jQuery UI Buttons & Button Sets
 	$(function () {
 		$('button[data-mod*="tsn8_button"]').button({
 			text: false
@@ -44,23 +10,23 @@ $(document).ready(function () {
 		$(".buttonset").buttonset();
 
 	});
-})
-
-function make_ajax_request(url, type, element_id) {
-
-	var request = $.ajax({
-		url: url,
-		type: type,
-		dataType: "html"
-	});
-
-	request.success(function (msg) {
-		insert_modal_contents(element_id, msg);
-	}).fail(function (jqXHR, textStatus) {
-		alert("Request failed: " + textStatus);
-	});
-}
-
-function insert_modal_contents(element_id, html) {
-	$('#' + element_id).html(html);
-}
+});
+//
+//function make_ajax_request(url, type, element_id) {
+//
+//	var request = $.ajax({
+//		url: url,
+//		type: type,
+//		dataType: "html"
+//	});
+//
+//	request.success(function (msg) {
+//		insert_modal_contents(element_id, msg);
+//	}).fail(function (jqXHR, textStatus) {
+//		alert("Request failed: " + textStatus);
+//	});
+//}
+//
+//function insert_modal_contents(element_id, html) {
+//	$('#' + element_id).html(html);
+//}
